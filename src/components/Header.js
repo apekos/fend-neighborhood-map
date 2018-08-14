6c154aa59ecf68961448253a0cd1ccb406895bc9
Alexandros Pekos <apekos7@gmail.com>
2018-08-14 04:06:41 +0300

    Create menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Header () {
-	return (
-		<header>
-			<h1>Athens Sightseeing</h1>
-		</header>
-	)
+class Header extends Component {
+
+	render() {
+		return (
+			<header>
+				<div className="header-title">
+					<h1>Athens Sightseeing</h1>
+				</div>
+				<div>
+					<a className="menu">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+							<path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"/>
+						</svg>
+					</a>
+				</div>
+			</header>
+		);
+	}
 }
 
 export default Header;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { compose, withProps } from 'recompose'
+import {  withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 class Map extends Component {
 
+
 	render() {
+		const GoogleMapExample = compose(
+			withProps({
+				googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDQsjBJv4iEpQ5rv6WsvD81pUKg2OqPwuM",
+				loadingElement: <div style={{ height: `100%` }} />,
+				containerElement: <div style={{ height: `100%` }} />,
+				mapElement: <div style={{ height: `100%` }} />,
+			}),
+			withScriptjs,
+			withGoogleMap
+		)((props) =>
+			<GoogleMap
+				defaultZoom={13}
+				defaultCenter={{ lat: 37.9838096, lng: 23.7275388 }}
+			>
+			</GoogleMap>
+		);
+
 		return (
 			<div className='map'>
-				Map
+				<GoogleMapExample />
 			</div>
 		);
 	}
