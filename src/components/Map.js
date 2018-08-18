1d4f120599d0e73969ee3435c865aaee883c43e1
Alexandros Pekos <apekos7@gmail.com>
2018-08-18 05:56:37 +0300

    fix: Stop map rerendering

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,18 +3,50 @@ import { compose, withProps } from 'recompose';
 import {  withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 // import Markers from './components/Markers.js';
 
-class Map extends Component {
+// const MyMap = withScriptjs(withGoogleMap((props) => (
+		 
+// 	<GoogleMap
+// 		defaultZoom={13}
+// 		defaultCenter={{ lat: 37.9838096, lng: 23.7275388 }}
+// 	>
+// 		{props.locations.map(l => {
+// 			return (
+// 				<Marker
+// 					key={l.id}
+// 					position={{ lat: l.location.lat, lng: l.location.lng }}
+// 				/>
+// 			)
+// 		})}
+// 	</GoogleMap>
+// )));
+
+// class Map extends Component {
+
+// 	render() {		
+// 		return (
+// 			<div className='map'>
+// 				<MyMap 
+// 					googleMapURL= "https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDQsjBJv4iEpQ5rv6WsvD81pUKg2OqPwuM"
+// 					loadingElement={<div style={{ height: `100%` }} />}
+// 					containerElement={<div style={{ height: `100%` }} />}
+// 					mapElement={<div style={{ height: `100%` }} />}
+// 					locations={this.props.locations}
+// 				/>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// export default Map;
 
 
-	render() {
-		const { locations } = this.props
-
-		const MyMap = compose(
+const MyMap = compose(
 			withProps({
 				googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDQsjBJv4iEpQ5rv6WsvD81pUKg2OqPwuM",
 				loadingElement: <div style={{ height: `100%` }} />,
 				containerElement: <div style={{ height: `100%` }} />,
 				mapElement: <div style={{ height: `100%` }} />,
+				//locations: this.props.locations,
 			}),
 			withScriptjs,
 			withGoogleMap
@@ -23,7 +55,7 @@ class Map extends Component {
 				defaultZoom={13}
 				defaultCenter={{ lat: 37.9838096, lng: 23.7275388 }}
 			>
-				{locations.map(l => {
+				{props.locations.map(l => {
 					return (
 						<Marker
 							key={l.id}
@@ -34,11 +66,18 @@ class Map extends Component {
 			</GoogleMap>
 		);
 
+class Map extends Component {
+
+
+	render() {
+
+		
+
 		return (
 			<div className='map'>
-				<MyMap>
-
-				</MyMap>
+				<MyMap 
+					locations={this.props.locations}
+				/>
 			</div>
 		);
 	}
